da1f06a0df2d76c17c27b2c0498c731b0aed166d
AayushGrg333 <aayushgrg333@gmail.com>
2024-12-08 21:36:46 +0545

    signin page initiallization

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,45 @@
-import React from 'react'
+'use client'
+import { ApiResponse } from '@/types/ApiResponse';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useDebounceCallback } from 'usehooks-ts'
+import * as z from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useToast } from "@/hooks/use-toast"
+import axios, { AxiosError } from 'axios';
+import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { signUpSchema } from '@/schemas/signUpSchema';
+import { signInSchema } from '@/schemas/signInSchema';
 
 function Signin() {
+  const [username,setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState('');
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const debounced = useDebounceCallback(setUsername, 300);
+
+  const { toast } = useToast();
+
+  const form = useForm({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    }
+  })
   return (
     <div>Signin</div>
   )
