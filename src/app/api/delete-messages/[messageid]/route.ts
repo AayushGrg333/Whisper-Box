58d793e93fc55d6c2da55508cdc1ca602531e3e1
AayushGrg333 <aayushgrg333@gmail.com>
2025-01-04 21:29:54 +0545

    worked in delte route

--- src/app/api/delete-messages/[messageid]/route.ts
+++ src/app/api/delete-messages/[messageid]/route.ts
@@ -6,9 +6,25 @@ import { User } from "next-auth";
 import mongoose from "mongoose";
 
 export async function DELETE(request: Request,{params}: {params: {messageid: string}}) {
+    const messageId = params.messageid
     await connectDb();
 
     const session = await getServerSession(authOptions);
-    const user: User = session?.user;
+    const user: User = session?.user as User;
+
+    if(!session || !user){
+        return Response.json({
+            success: false,
+            message: "User not authenicated",
+        },{status: 401})
+    };
+
+    try {
+        const updatedResult = await userModel.updateOne({_id: user._id},{
+            $pull : {messages : { _id : messageId}}
+        })
+    } catch (error) {
+        
+    }
 
 }
