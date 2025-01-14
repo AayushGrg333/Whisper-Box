5ab76f36f927a6a9fed46a97cf272f895e4ecc87
AayushGrg333 <aayushgrg333@gmail.com>
2025-01-14 23:00:29 +0545

    almost dont with send page

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -2,87 +2,142 @@
 
 import { messageSchema } from "@/schemas/messageSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import axios,{AxiosError} from "axios";
+import axios, { AxiosError } from "axios";
 import { ApiResponse } from "@/types/ApiResponse";
 import { useToast } from "@/hooks/use-toast";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useDebounceCallback } from "usehooks-ts";
 
 function SendMessage() {
-    const [message, setMessages] = useState("");
+    const [message, setMessage] = useState<string>("");
+    const [messageError, setMessageError] = useState<string | null>(null);
     const [isSendingMessage, setIsSendingMessage] = useState<boolean>(false);
-    const [AiSuggestion,setAISuggestion] = useState<string[]>([])
-    const [loadingAiSuggestion,setLoadingAiSuggestion] = useState<boolean>(false)
+    const [AiSuggestion, setAISuggestion] = useState<string[]>([]);
+    const [loadingAiSuggestion, setLoadingAiSuggestion] = useState<boolean>(false);
 
-    const {toast} = useToast();
+    const { toast } = useToast();
 
     //extracting the username
     const pathname = usePathname();
     const username = pathname.split("/")[2];
 
-    const form = useForm({
+    const form = useForm<z.infer<typeof messageSchema>>({
         resolver: zodResolver(messageSchema),
+        defaultValues: {
+            content: "",
+        },
     });
 
+    const result = messageSchema.safeParse({ content: message });
+    if (!result.success) {
+        const errorMessage = result.error.errors[0]?.message;
+        setMessageError(errorMessage);
+    } else {
+        setMessageError(null);
+    }
+
     const handleSendMessage = async () => {
-      setIsSendingMessage(true)
+        setIsSendingMessage(true);
         try {
             const response = await axios.post<ApiResponse>(
                 "/api/send-message",
                 {
-                  username : username,
-                  content : message,
+                    username: username,
+                    content: message,
                 }
             );
-            console.log(response.data.message)
+            console.log(response.data.message);
             toast({
                 title: "Success",
                 description: response.data.message,
             });
         } catch (error) {
-          console.error("Error during sening message",error);
-          const axiosError = error as AxiosError<ApiResponse>
+            console.error("Error during sening message", error);
+            const axiosError = error as AxiosError<ApiResponse>;
 
-          let errormessage = axiosError.response?.data.message;
+            let errormessage = axiosError.response?.data.message;
 
-          toast({
-            title: 'failed sending message',
-            description: errormessage,
-            variant: 'destructive',
-          });
-        }finally{
-          setIsSendingMessage(false);
+            toast({
+                title: "failed sending message",
+                description: errormessage,
+                variant: "destructive",
+            });
+        } finally {
+            setIsSendingMessage(false);
         }
-        
     };
 
-    const handleSuggestMessage = async () =>{
-      setLoadingAiSuggestion(true)
-      try {
-        const response = await axios.get<ApiResponse>("/api/suggest-messages");
-        const suggestion = response.data.text as string;
-        const suggestionArr = suggestion?.split('||');
-        setAISuggestion(suggestionArr)
-
-      } catch (error) {
-        console.error("Error duing getting message suggestion",error);
-        const axiosError = error as AxiosError<ApiResponse>
-        let errormessage = axiosError.response?.data.message;
-        toast({
-          title : "failed getting message suggestion",
-          description: errormessage,
-          variant: 'destructive',
-        })
-      } finally{
-        setLoadingAiSuggestion(false);
-      }
-    }
+    const handleSuggestMessage = async () => {
+        setLoadingAiSuggestion(true);
+        try {
+            const response = await axios.get<ApiResponse>(
+                "/api/suggest-messages"
+            );
+            const suggestion = response.data.text as string;
+            const suggestionArr = suggestion?.split("||");
+            setAISuggestion(suggestionArr);
+        } catch (error) {
+            console.error("Error duing getting message suggestion", error);
+            const axiosError = error as AxiosError<ApiResponse>;
+            let errormessage = axiosError.response?.data.message;
+            toast({
+                title: "failed getting message suggestion",
+                description: errormessage,
+                variant: "destructive",
+            });
+        } finally {
+            setLoadingAiSuggestion(false);
+        }
+    };
 
     return (
         <>
-            <Button onClick={handleSendMessage}>send</Button><br />
+            <div className="">
+                <div className="text-3xl">Send Anonymous Message</div>
+                <div>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(handleSendMessage)}
+                            className="space-y-8"
+                        >
+                            <FormField
+                                control={form.control}
+                                name="content"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>
+                                            Ask me a Question:
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="ask me here..."
+                                                {...field}
+                                              
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit">Submit</Button>
+                        </form>
+                    </Form>
+                </div>
+            </div>
             <Button onClick={handleSuggestMessage}>getsuggestion</Button>
         </>
     );
--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const messageSchema = z.object({
-    content:z
-    .string()
-    .min(10,{message:"content must be atlest 10 charaters "})
-    .max(200,{message: "content must be no longer thant  300 charaters"})
-});
-
+    content: z
+      .string()
+      .min(10, { message: "Content must be at least 10 characters." })
+      .max(200, { message: "Content must be no longer than 200 characters." }),
+  });
+  
