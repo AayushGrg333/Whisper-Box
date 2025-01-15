338cfa64ad28a72666721775a8670f51582faacb
AayushGrg333 <aayushgrg333@gmail.com>
2025-01-15 09:25:14 +0545

    done with ai suggestion

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -38,8 +38,6 @@ function SendMessage() {
             content: "",
         },
     });
-    
-    
 
     const handleSendMessage = async (data: z.infer<typeof messageSchema>) => {
         setIsSendingMessage(true);
@@ -72,33 +70,40 @@ function SendMessage() {
         }
     };
 
-    const handleSuggestMessage = async () => {
-        setLoadingAiSuggestion(true);
-        try {
-            const response = await axios.get<ApiResponse>(
-                "/api/suggest-messages"
-            );
-            const suggestion = response.data.text as string;
-            const suggestionArr = suggestion?.split("||");
-            setAISuggestion(suggestionArr);
-        } catch (error) {
-            console.error("Error duing getting message suggestion", error);
-            const axiosError = error as AxiosError<ApiResponse>;
-            let errormessage = axiosError.response?.data.message;
-            toast({
-                title: "failed getting message suggestion",
-                description: errormessage,
-                variant: "destructive",
-            });
-        } finally {
-            setLoadingAiSuggestion(false);
-        }
-    };
+    useEffect(() => {
+        const handleSuggestMessage = async () => {
+            setLoadingAiSuggestion(true);
+            try {
+                const response = await axios.get<ApiResponse>(
+                    "/api/suggest-messages"
+                );
+                const suggestion = response.data.text as string;
+                const suggestionArr = suggestion?.split("||");
+                setAISuggestion(suggestionArr);
+            } catch (error) {
+                console.error("Error duing getting message suggestion", error);
+                const axiosError = error as AxiosError<ApiResponse>;
+                let errormessage = axiosError.response?.data.message;
+                toast({
+                    title: "failed getting message suggestion",
+                    description: errormessage,
+                    variant: "destructive",
+                });
+            } finally {
+                setLoadingAiSuggestion(false);
+            }
+        };
+        handleSuggestMessage()
+    }, [])
+
+    const handleSuggestionClick = (suggestion : string) =>{
+        form.setValue("content",suggestion);
+    }
 
     return (
         <>
-            <div className="flex justify-center items-center min-h-screen bg-grey">
-                <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg ">
+            <div className="flex justify-center flex-col items-center min-h-screen bg-grey">
+                <div className="w-full max-w-2xl p-8 space-y-8 bg-white rounded-lg ">
                 <div className="text-center">
                     <h1 className="text-3xl font-bold trackeing-tight lg:text-4xl">Send Anonymous Message to {username}</h1>
                 </div>
@@ -120,7 +125,6 @@ function SendMessage() {
                                             <Input
                                                 placeholder="ask me here..."
                                                 {...field}
-                                              
                                             />
                                         </FormControl>
                                         <FormMessage>{form.formState.errors.content?.message}</FormMessage>
@@ -140,6 +144,33 @@ function SendMessage() {
                     </Form>
                 </div>
                 </div>
+
+                <div>
+                    {loadingAiSuggestion ? (
+                        <>
+                        <Loader2 className="mr-2 h-5 w-5 animate-spin"/>Loading ai Suggestion...
+                        </>
+                    ):(
+                        <div className="bg-slate-200 max-w-4xl flex flex-col justify-center items-center w-full p-8 space-y-8 rounded-lg">
+                        <h2 className="text-3xl">AI Generated suggestions:</h2>
+                        <div className="flex flex-col justify-center space-y-5">
+                                    {
+                                        AiSuggestion.map((AiSuggestion,index)=>(
+                                            <Button 
+                                            key={index}
+                                            variant='outline'
+                                            onClick={()=>handleSuggestionClick(AiSuggestion)}
+                                            >
+                                                {AiSuggestion}
+                                            </Button>
+                                            
+                                        ))
+                                    }
+                        </div>
+                    </div>
+                    )}
+                </div>
+
             </div>
         </>
     );
