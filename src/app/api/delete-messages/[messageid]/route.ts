116dcdfa0f0e28b9bc32759f30b4bc5c46f37482
AayushGrg333 <aayushgrg333@gmail.com>
2025-01-16 08:54:51 +0545

    more errors

--- src/app/api/delete-messages/[messageid]/route.ts
+++ src/app/api/delete-messages/[messageid]/route.ts
@@ -2,63 +2,64 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../auth/[...nextauth]/options";
 import userModel from "@/models/user";
 import connectDb from "@/lib/connnectdb";
+import mongoose from "mongoose";
 
 export async function DELETE(
   request: Request,
-  { params }: { params: { messageid: string } }
+  context: { params: { messageid: string } }
 ) {
-  const messageId = params.messageid;
+  const { messageid } = context.params;
 
-  connectDb();
+  await connectDb();
 
   const session = await getServerSession(authOptions);
   const user = session?.user;
 
   if (!session || !user) {
-    return Response.json(
-      {
+    return new Response(
+      JSON.stringify({
         success: false,
         message: "Not Authenticated",
-      },
-      { status: 401 }
+      }),
+      { status: 401, headers: { "Content-Type": "application/json" } }
     );
   }
 
+  const userId = new mongoose.Types.ObjectId(user._id); //convert string into objectid
   try {
     const updatedResult = await userModel.updateOne(
       {
-        _id: user._id,
+        _id: userId,
       },
       {
-        $pull: { messages: { _id: messageId } },
+        $pull: { messages: { _id: messageid } },
       }
     );
 
     if (updatedResult.modifiedCount == 0) {
-      return Response.json(
-        {
+      return new Response(
+        JSON.stringify({
           success: false,
           message: "Message not found or already deleted",
-        },
-        { status: 404 }
+        }),
+        { status: 404, headers: { "Content-Type": "application/json" } }
       );
     }
 
-    return Response.json(
-      {
+    return new Response(
+      JSON.stringify({
         success: true,
         message: "Message Deleted Successfully",
-      },
-      { status: 200 }
+      }),
+      { status: 200, headers: { "Content-Type": "application/json" } }
     );
   } catch (error) {
-    console.log("Error deleting message", error);
-    return Response.json(
-      {
+    return new Response(
+      JSON.stringify({
         success: false,
         message: "Error deleting message",
-      },
-      { status: 500 }
+      }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
     );
   }
 }
